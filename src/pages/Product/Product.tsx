6b5832785a9d227b446685ba2420bafe8c6b882b
Erkhan Denis <erhan.denis@list.ru>
2023-04-01 18:19:22 +0500

    feat: add tyled product page

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,15 +1,64 @@
-import { useParams } from "react-router-dom";
-import { useProductList } from "../../store/hooks/use-store-actions";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+import { TProduct } from "../../store/reducers/types";
+
+import { PRODUCTS_LIST } from "../Home/_constants";
+
+import "./Product.scss";
 
 const Product = () => {
   const { id } = useParams();
-  const { productList } = useProductList();
-  const product = productList.find((product) => product.id === Number(id));
+  const [currentProduct, setCurrentProduct] = useState<TProduct>();
+
+  useEffect(() => {
+    (async () => {
+      const respounse = await fetch(`${PRODUCTS_LIST}/${id}`);
+      const product: TProduct = await respounse.json();
+      setCurrentProduct(product);
+    })();
+  }, []);
+
+  if (!currentProduct) {
+    return <h2>Not product</h2>;
+  }
 
   return (
-    <div className="product-info">
-      <h5 className="product-title">{product?.title}</h5>
-    </div>
+    <main className="card">
+      <Link to="/" className="back-link">
+        Back to products
+      </Link>
+      <section className="card_wrapper">
+        <div className="card-info">
+          <h1 className="card_title">{currentProduct.title}</h1>
+          <h2 className="card_price">
+            <span className="card_price__subtitle">Price: </span>
+            <span className="card_info__value">${currentProduct.price}</span>
+          </h2>
+          <p className="card_discription">
+            <span className="card_discription__value">
+              {currentProduct.description}
+            </span>
+          </p>
+          <p className="card_info">
+            <span className="card_info__subtitle">Category: </span>
+            <span className="card_info__value">{currentProduct.category}</span>
+          </p>
+          <p className="card_info">
+            <span className="card_info__subtitle">Brand: </span>
+            <span className="card_info__value">{currentProduct.brand}</span>
+          </p>
+          <p className="card_info">
+            <span className="card_info__subtitle">Rating: </span>
+            <span className="card_info__value">{currentProduct.rating}</span>
+          </p>
+          <p className="card_info">
+            <span className="card_info__subtitle">Stock: </span>
+            <span className="card_info__value">{currentProduct.stock}</span>
+          </p>
+        </div>
+      </section>
+    </main>
   );
 };
 
