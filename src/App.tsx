f8df58d1006e053f8aff93337be9a800e55947c1
Erkhan Denis <erhan.denis@list.ru>
2023-04-01 18:28:49 +0500

    refactor: App refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
-import { useEffect } from "react";
-
-import { useProductList } from "./store/hooks/use-store-actions";
-import { useFilter } from "./store/hooks/use-filter-actions";
-
 import AppRouter from "./router/AppRouter";
-
-import { PRODUCTS_LIST } from "./pages/Home/_constants";
-
-import { TProduct } from "./store/reducers/types";
 
 import "./App.scss";
 
 const App = () => {
-  const { setProductList } = useProductList();
-  const { searchTitle, searchCategory, searchBrand } = useFilter();
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const respounse = await fetch(PRODUCTS_LIST);
-        const products: TProduct[] = await respounse.json();
-
-        setProductList(
-          products.filter(
-            (product) =>
-              product.title.toLowerCase().includes(searchTitle.toLowerCase()) &&
-              product.brand.toLowerCase().includes(searchBrand) &&
-              (searchCategory.length
-                ? searchCategory.some(
-                    (category) => product.category.toLowerCase() === category
-                  )
-                : true)
-          )
-        );
-      } catch (error) {
-        throw new Error("Can't find products");
-      }
-    })();
-  }, [searchTitle, searchCategory, searchBrand]);
-
   return <AppRouter />;
 };
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,44 @@
+import { useEffect } from "react";
+
+import { useFilter } from "../../store/hooks/use-filter-actions";
+import { useProductList } from "../../store/hooks/use-store-actions";
+
 import FiltersBlock from "./components/FiltersBlock/FiltersBlock";
 import ProductList from "./components/ProductList/ProductList";
+
+import { TProduct } from "../../store/reducers/types";
+
+import { PRODUCTS_LIST } from "./_constants";
 
 import "./Home.scss";
 
 const Home = () => {
+  const { setProductList } = useProductList();
+  const { searchTitle, searchCategory, searchBrand } = useFilter();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const respounse = await fetch(PRODUCTS_LIST);
+        const products: TProduct[] = await respounse.json();
+
+        setProductList(
+          products.filter(
+            (product) =>
+              product.title.toLowerCase().includes(searchTitle.toLowerCase()) &&
+              product.brand.toLowerCase().includes(searchBrand) &&
+              (searchCategory.length
+                ? searchCategory.some(
+                    (category) => product.category.toLowerCase() === category
+                  )
+                : true)
+          )
+        );
+      } catch (error) {
+        throw new Error("Can't find products");
+      }
+    })();
+  }, [searchTitle, searchCategory, searchBrand]);
   return (
     <main className="main">
       <ProductList />
